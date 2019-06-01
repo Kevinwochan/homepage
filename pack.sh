c240a0ce0a2be098832f774faf39d1b86f342fa7
Kevin <kevinwochan@gmail.com>
2019-06-02 04:15:02 +1000

    added background

--- pack.sh
+++ pack.sh
@@ -1,1 +1,1 @@
-zip -u Material-Dashboard.zip dashboard.html images/*.png js/*.min.js css/*.min.css fonts/* manifest.json
+zip -u Material-Dashboard.zip dashboard.html images/* js/*.min.js css/*.min.css fonts/* manifest.json
